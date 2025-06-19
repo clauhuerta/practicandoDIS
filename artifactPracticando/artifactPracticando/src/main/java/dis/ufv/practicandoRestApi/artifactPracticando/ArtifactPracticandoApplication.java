205dc4bedd62be97558211b1c45b3d5c5dc65213
clauhuerta <claudiahr02@icloud.com>
2025-06-19 18:24:33 +0200

    añadido carpeta Api

---
+++ artifactPracticando/artifactPracticando/src/main/java/dis/ufv/practicandoRestApi/artifactPracticando/ArtifactPracticandoApplication.java
@@ -0,0 +1,13 @@
+package dis.ufv.practicandoRestApi.artifactPracticando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactPracticandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactPracticandoApplication.class, args);
+	}
+
+}
---
+++ artifactPracticando/artifactPracticando/src/test/java/dis/ufv/practicandoRestApi/artifactPracticando/ArtifactPracticandoApplicationTests.java
@@ -0,0 +1,13 @@
+package dis.ufv.practicandoRestApi.artifactPracticando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactPracticandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
